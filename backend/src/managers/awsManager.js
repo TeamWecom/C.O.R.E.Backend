ada18dac1c4d3c7b570b99b5f3cf7d30d7cbca6a
Danilo <daniloevolz@gmail.com>
2024-12-18 16:43:24 -0300

    Update SDK AWS

--- backend/src/managers/awsManager.js
+++ backend/src/managers/awsManager.js
@@ -1,48 +1,44 @@
-import  AWS  from 'aws-sdk';
-import { log } from '../utils/log.js';
-import process from 'process';
+import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
+import { log } from "../utils/log.js";
+import process from "process";
 
-export const sendSms = (cgpn, msg) =>{
-    return new Promise((resolve, reject) => {
-        try{
-            if(cgpn && cgpn.length >10){//cgpn == '051999958798' || cgpn == '051995418797'
-                log('awsManager:sendSms: Número cgpn para ser notificado '+ cgpn.replace(/^\d/, '+55'))
-                //Vai enviar SMS
-                // Set region
-                // Configurar o AWS SDK com as suas credenciais e região
-                AWS.config.update({
-                accessKeyId: process.env.AWS_ACCESSKEY_ID,
-                secretAccessKey: process.env.AWS_ACCESSKEY_SECRET,
-                region: process.env.AWS_REGION
-                });
-        
-                // Create publish parameters
-                const params = {
-                Message: msg,
-                PhoneNumber: cgpn.replace(/^\d/, '+55'),
-                };
-        
-                // Create promise and SNS service object
-                const publishTextPromise = new AWS.SNS({ apiVersion: "2010-03-31" })
-                .publish(params)
-                .promise();
-        
-                // Handle promise's fulfilled/rejected states
-                publishTextPromise
-                .then(function (data) {
-                    log("awsManager:sendSms: MessageID is " + data.MessageId);
-                    resolve(`Mensagem enviada ID ${data.MessageId}`);
-                })
-                .catch(function (err) {
-                    log(`awsManager:sendSms: SNS Erro ${err}`);
-                    reject(`awsManager:sendSms: SNS Erro ${err}`);
-                });
-            }else{
-                reject(`awsManager:sendSms: Número inválido`)
-            }
-        }catch(e){
-            log(`awsManager:sendSms: Erro ${e}`)
-            reject(`awsManager:sendSms: Erro ${e}`);
-        }    
-    });
-}
+// Configuração do cliente SNS
+const snsClient = new SNSClient({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESSKEY_ID,
+    secretAccessKey: process.env.AWS_ACCESSKEY_SECRET,
+  },
+});
+
+export const sendSms = (cgpn, msg) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (cgpn && cgpn.length > 10) {
+        log("awsManager:sendSms: Número cgpn para ser notificado " + cgpn.replace(/^\d/, "+55"));
+
+        // Parâmetros do comando SNS
+        const params = {
+          Message: msg,
+          PhoneNumber: cgpn.replace(/^\d/, "+55"), // Ajustando o número para +55 formato internacional
+        };
+
+        try {
+          const command = new PublishCommand(params);
+          const response = await snsClient.send(command);
+
+          log("awsManager:sendSms: MessageID is " + response.MessageId);
+          resolve({success:response.MessageId});
+        } catch (err) {
+          log(`awsManager:sendSms: SNS Erro ${err}`);
+          reject({error: `awsManager:sendSms: SNS Erro ${err}`});
+        }
+      } else {
+        reject({error:`awsManager:sendSms: Número inválido`});
+      }
+    } catch (e) {
+      log(`awsManager:sendSms: Erro ${e}`);
+      reject({error:`awsManager:sendSms: Erro ${e}`});
+    }
+  });
+};
